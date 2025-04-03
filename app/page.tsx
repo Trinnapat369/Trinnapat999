01a2a03fe5447b502c5d5fd601a680f994bc9ea9
Trinnapat Naksan <omobily6578@gmail.com>
2025-04-03 11:28:59 +0700

    first commit”

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,45 @@
+import Image from 'next/image'
+
 export default function MyApp() {
-  return (
-    <div>Hello word!</div>
-  )
+  return (<div 
+    className="w-[80%] border rounded p-4 m-auto bg-slate-100 
+     grid 
+    grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-4"
+    >
+      {/* grid-cols-1 gap-4 
+      sm:grid-cols-2
+      md:grid-cols-3
+      lg:grid-cols-4
+      xl:grid-cols-5
+
+      flex flexwrap */}
+
+    <h1 className="text-2xl font-bold m-2 p-2">Hello world!!</h1>
+
+    <Image
+      className='p-2 m-4'
+      src="https://computing.psu.ac.th/th/wp-content/uploads/2023/09/PSU-CoC-ENG_01_x49.png"
+      width={150}
+      height={150}
+      alt="PSU Logo"
+    />
+
+    <Image
+      className="p-2 m-4"
+      src="/next.svg"
+      width={150}
+      height={150}
+      alt="Next.js Logo"
+    />
+
+    <div className="w-64 p-2 border mb-2 rounded bg-blue-200">
+      Lorem idivsum, dolor sit amet consectetur adidivisicing elit. Doloribus, consequuntur.
+    </div>
+    <div className="w-64 p-2 border mb-2 rounded bg-blue-600 text-white">
+      Lorem idivsum, dolor sit amet consectetur adidivisicing elit. Doloribus, consequuntur.
+    </div>
+    <div className="w-64 p-2 border rounded bg-amber-300">
+      Lorem idivsum, dolor sit amet consectetur adipisicing elit. Doloribus, consequuntur.
+    </div>
+  </div>)
 }

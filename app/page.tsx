3c91410fd875cb855ebefe3e877ff9f149011b36
Trinnapat Naksan <omobily6578@gmail.com>
2025-04-04 10:14:03 +0700

    first commit”

--- app/page.tsx
+++ app/page.tsx
@@ -1,51 +1,53 @@
 import Image from 'next/image'
 import { Bungee_Tint } from "next/font/google";
+import Link from 'next/link';
 
-const bungee = Bungee_Tint({ 
+const bungee = Bungee_Tint({
   subsets: ["latin"],
   weight: "400",
 });
 
 export default function MyApp() {
-  return (<div 
-    className="w-[80%] border rounded p-4 m-auto bg-slate-100 
+  return (<div>  
+    <h1 className={`${bungee.className} "font-bold m-2 p-2"`}>Hello world!!</h1>
+
+      <Link 
+        className='text-blue-800 block border-2 border-black p-2 m-2 w-fit mb-8'
+        href="/products" 
+        >
+          Go to Product Page
+      </Link>
+
+
+    <div className="w-[80%] border rounded p-4 m-auto bg-slate-100 
      grid 
-    grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-4"
-    >
-      {/* grid-cols-1 gap-4 
-      sm:grid-cols-2
-      md:grid-cols-3
-      lg:grid-cols-4
-      xl:grid-cols-5
+    grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-4">
+      <Image
+        className='p-2 m-4'
+        src="https://computing.psu.ac.th/th/wp-content/uploads/2023/09/PSU-CoC-ENG_01_x49.png"
+        width={150}
+        height={150}
+        alt="PSU Logo"
+      />
 
-      flex flexwrap */}
+      <Image
+        className="p-2 m-4"
+        src="/next.svg"
+        width={150}
+        height={150}
+        alt="Next.js Logo"
+      />
 
-    <h1 className={`${bungee.className} "text-4xl font-bold m-2 p-2"`}>Hello world!!</h1>
-
-    <Image
-      className='p-2 m-4'
-      src="https://computing.psu.ac.th/th/wp-content/uploads/2023/09/PSU-CoC-ENG_01_x49.png"
-      width={150}
-      height={150}
-      alt="PSU Logo"
-    />
-
-    <Image
-      className="p-2 m-4"
-      src="/next.svg"
-      width={150}
-      height={150}
-      alt="Next.js Logo"
-    />
-
-    <div className="w-64 p-2 border mb-2 rounded bg-blue-200">
-      Lorem idivsum, dolor sit amet consectetur adidivisicing elit. Doloribus, consequuntur.
+      <div className="w-64 p-2 border mb-2 rounded bg-blue-200 hover:bg-blue-700 hover:text-blue-50">
+        Lorem idivsum, dolor sit amet consectetur adidivisicing elit. Doloribus, consequuntur.
+      </div>
+      <div className="w-64 p-2 border mb-2 rounded bg-blue-600 text-white">
+        Lorem idivsum, dolor sit amet consectetur adidivisicing elit. Doloribus, consequuntur.
+      </div>
+      <div className="w-64 p-2 border rounded bg-amber-300">
+        Lorem idivsum, dolor sit amet consectetur adipisicing elit. Doloribus, consequuntur.
+      </div>
     </div>
-    <div className="w-64 p-2 border mb-2 rounded bg-blue-600 text-white">
-      Lorem idivsum, dolor sit amet consectetur adidivisicing elit. Doloribus, consequuntur.
-    </div>
-    <div className="w-64 p-2 border rounded bg-amber-300">
-      Lorem idivsum, dolor sit amet consectetur adipisicing elit. Doloribus, consequuntur.
-    </div>
+
   </div>)
 }
---
+++ app/products/page.tsx
@@ -0,0 +1,30 @@
+export default function Products() {
+    return (
+        <div>
+            <h1 className="font-bold text-xl m-4">Product</h1>
+            <div className="flex flex-wrap m-auto justify-center w-[80%] border rounded m-4 p-4 bg-slate-100 mb-6">
+                <div className="shadow-lg rounded-md border w-fit bg-green-200 p-2 m-2">
+                    <h2>1. Computer:</h2>
+                    <p>Product description goes here.</p>
+                    <button className="border bg-blue-200 px-2 py-1 mt-2 rounded hover:font-bold">Buy Now</button>
+                </div>
+                <div className="shadow-lg rounded-md border w-fit bg-green-200 p-2 m-2">
+                    <h2>2. Laptop core i7:</h2>
+                    <p>Product description goes here.</p>
+                    <button className="border bg-blue-200 px-2 py-1 mt-2 rounded hover:font-bold">Buy Now</button>
+                </div>
+                <div className="shadow-lg rounded-md border w-fit bg-green-200 p-2 m-2">
+                    <h2>2. Laptop core i7:</h2>
+                    <p>Product description goes here.</p>
+                    <button className="border bg-blue-200 px-2 py-1 mt-2 rounded hover:font-bold">Buy Now</button>
+                </div>
+                <div className="shadow-lg rounded-md border w-fit bg-green-200 p-2 m-2">
+                    <h2>2. Laptop core i7:</h2>
+                    <p>Product description goes here.</p>
+                    <button className="border bg-blue-200 px-2 py-1 mt-2 rounded hover:font-bold">Buy Now</button>
+                </div>
+            </div>
+            <footer className="bg-blue-200 p-2 text-center font-bold text-xl"> ..:: Product footer ::.. </footer>
+        </div>
+    )
+}
